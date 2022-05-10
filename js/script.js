cf4b88604c6676d6ef231510a0d92c42b506d89a
LucasJFoster <105177539+LucasJFoster@users.noreply.github.com>
2022-05-10 14:13:22 +1200

    Fixs plus webpage icon

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-function buttonClicked(){
-    alert ('i am clicked');
-}
-function buttontochnagetextclicked(){
-    document.getElementById('targetTag').innerHTML = "Javascript text";
-}
+
+$(function(){
+    var pages = ['index', 'about', 'contact' ];
+    var pathname = window.location.pathname;
+
+    $('.nav-link').each(function(i){
+     if(pathname.includers(pages[i])){
+          $(this).addClass('active');
+          $(this).attr('aria-current', 'page');
+     }else if(this.className.includes('active')){
+          $(this).removeClass('active');
+     }
+    });
+});
